28907472961556f709c8eb8b8421b4488ebd4066
semal17 <senia-2010@mail.ru>
2020-09-17 18:37:18 +0300

    сверстал почти всю главную страницу

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 $(function(){
+$('.photo-slider').slick({
+    
+    prevArrow: '<button type="button" class="slick-prev"><img src="img/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="img/arrow-right.svg" alt=""></button>'
+});
 
 });
